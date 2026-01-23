7d8d05fe1e2f549d416c7206f1352a47c8340667
shabana_asthma <36826017+rupalimo@users.noreply.github.com>
2026-01-23 10:30:30 -0500

    first commit, add website files

---
+++ js/code-table-freshkill.js
@@ -0,0 +1,138 @@
+var SPREADSHEET_ID_AND_TAB =
+	'1qg2NQO9Qh_zjApjyMTMLcUorq8Ox2Al8x6_a1q42gSg/freshkill';
+
+$(document).ready(function () {
+	$.getJSON(
+		'https://opensheet.elk.sh/' + SPREADSHEET_ID_AND_TAB,
+		function (data) {
+			console.log(data);
+
+			data.forEach(function (entry, index) {
+				console.log(entry);
+
+				if (index == 0) return;
+
+				let ul = $(`<p>` + entry.Tags + `</p>`).appendTo('#tags');
+
+				if (entry.Ready == 'show') {
+					let section = $(
+						`<span><h5>` +
+							entry.Title +
+							`</h5><h5>` +
+							entry.Director +
+							`</h5><h5>` +
+							entry.Year +
+							`</h5></span`
+					).appendTo('#banner-title');
+
+					let div = $(
+						`<p>` +
+							entry.EventDate +
+							`</p><p>` +
+							entry.EventLocation +
+							`</p><a data-confirm="` +
+							entry.TicketLink +
+							`" src="` +
+							entry.TicketLink +
+							`">` +
+							entry.TicketPrice +
+							`</a><a src="` +
+							entry.CopresenterLink +
+							`">` +
+							entry.Copresenter +
+							`</a><p>` +
+							entry.Collaborators +
+							`</p>`
+					).appendTo('#banner-details');
+
+					let img = $(
+						`<img class="b__img" src="../film-stills/` +
+							entry.FilmStills +
+							`1.jpg" title="Film still: ` +
+							entry.Title +
+							`, ` +
+							entry.Year +
+							`" />`
+					).appendTo('#banner-img');
+				}
+				let div = $(
+					`<p data-confirm="` +
+						entry.Duration +
+						`">` +
+						entry.Duration +
+						`</p><p data-confirm="` +
+						entry.Country +
+						`">` +
+						entry.Country +
+						`</p><p data-confirm="` +
+						entry.Language +
+						`">` +
+						entry.Language +
+						`</p><a href="` +
+						entry.TrailerLink +
+						`" data-confirm="` +
+						entry.TrailerLink +
+						`" target="_blank">Trailer</a>`
+				).appendTo('#film-meta');
+
+				let p = $(
+					`<p data-confirm="` +
+						entry.EventDescription +
+						`">` +
+						entry.EventDescription +
+						`</p>`
+				).appendTo('#film-description');
+
+				let span = $(
+					`<p data-confirm="` +
+						entry.DirectorBio +
+						`">` +
+						entry.DirectorBio +
+						`</p>`
+				).appendTo('#dir-bio');
+
+				let article = $(
+					`<img data-confirm="` +
+						entry.FilmStills +
+						`" class="secondary__img" src="../film-stills/` +
+						entry.FilmStills +
+						`2.jpg" title="Film still: ` +
+						entry.Title +
+						`, ` +
+						entry.Year +
+						`" /><img data-confirm="` +
+						entry.FilmStills +
+						`" class="secondary__img" src="../film-stills/` +
+						entry.FilmStills +
+						`3.jpg" title="Film still: ` +
+						entry.Title +
+						`, ` +
+						entry.Year +
+						`" /><img data-confirm="` +
+						entry.FilmStills +
+						`" class="secondary__img" src="../film-stills/` +
+						entry.FilmStills +
+						`4.jpg" title="Film still: ` +
+						entry.Title +
+						`, ` +
+						entry.Year +
+						`" />`
+				).appendTo('#film-stills');
+
+				let footer = $(
+					`<div  data-confirm="` +
+						entry.SeriesTitle +
+						`"><p data-confirm="` +
+						entry.SeriesDescription +
+						`">` +
+						entry.SeriesDescription +
+						`</p><p data-confirm="` +
+						entry.SeriesProgram +
+						`">` +
+						entry.SeriesProgram +
+						`</p></div>`
+				).appendTo('#series');
+			});
+		}
+	);
+});
---
+++ js/code-table.js
@@ -0,0 +1,97 @@
+var SPREADSHEET_ID_AND_TAB =
+	'1qg2NQO9Qh_zjApjyMTMLcUorq8Ox2Al8x6_a1q42gSg/current';
+
+$(document).ready(function () {
+	$.getJSON(
+		'https://opensheet.elk.sh/' + SPREADSHEET_ID_AND_TAB,
+		function (data) {
+			console.log(data);
+
+			data.forEach(function (entry, index) {
+				console.log(entry);
+
+				if (index == 0) return;
+
+				let section = $(
+					`<a href="#" class="program--item" data-confirm="` +
+						entry.Ready +
+						`" data-upcoming="` +
+						entry.TagUpcoming +
+						`" data-category="all ` +
+						entry.EventType +
+						`"><ul>
+					<li data-filter-type="` +
+						entry.TagSeries +
+						`" class="filter--series">` +
+						entry.SeriesTitle +
+						`</li>
+				</ul><img src="film-posters/` +
+						entry.ImgFileName +
+						`.jpg" /><div class="program--details"><h4>` +
+						entry.Title +
+						`</h4><span><h5>` +
+						entry.Director +
+						`</h5><p data-year="` +
+						entry.Year +
+						`">` +
+						entry.Year +
+						`</p></span></div><span data-filter-type="` +
+						entry.TagUpcoming +
+						`" class="filter--upcoming"><p>` +
+						entry.EventDate +
+						`</p><p>` +
+						entry.EventLocation +
+						`</p></span><p data-online="` +
+						entry.OnlineIrl +
+						`" class="filter--online">` +
+						entry.OnlineIrl +
+						`</p></a>`
+				).appendTo('#listings');
+			});
+		}
+	);
+
+	var $filterRadio = $('input[type="radio"]');
+	var filterFunc = function () {
+		var selectedFilters = {};
+
+		$filterRadio.filter(':checked').each(function () {
+			if (!selectedFilters.hasOwnProperty(this.name)) {
+				selectedFilters[this.name] = [];
+			}
+
+			selectedFilters[this.name].push(this.value);
+		});
+
+		// create a collection containing all of the filterable elements
+		var $filteredResults = $('.program--item');
+
+		// loop over the selected filter name -> (array) values pairs
+		$.each(selectedFilters, function (name, filterValues) {
+			// filter each .program--item element
+			$filteredResults = $filteredResults.filter(function () {
+				var matched = false,
+					currentFilterValues = $(this).data('category').split(' ');
+
+				// loop over each category value in the current .program--item's data-category
+				$.each(currentFilterValues, function (_, currentFilterValue) {
+					// if the current category exists in the selected filters array
+					// set matched to true, and stop looping. as we're ORing in each
+					// set of filters, we only need to match once
+
+					if ($.inArray(currentFilterValue, filterValues) != -1) {
+						matched = true;
+						return false;
+					}
+				});
+
+				// if matched is true the current .program--item element is returned
+				return matched;
+			});
+		});
+
+		$('.program--item').hide().filter($filteredResults).show();
+	};
+
+	$filterRadio.on('change', filterFunc);
+});
