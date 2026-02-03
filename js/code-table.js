dba771df054e672ae63260e2f285bd0f494d5f52
shabana_asthma <36826017+rupalimo@users.noreply.github.com>
2026-02-03 13:48:11 -0500

    remove hover for non links

--- js/code-table.js
+++ js/code-table.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
 				let section = $(
 					`<a href="` +
-						entry.Ready +
+						entry.SecondaryPage +
 						`" class="program--item" data-confirm="` +
 						entry.Ready +
 						`" data-upcoming="` +
